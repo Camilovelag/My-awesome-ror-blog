1d0e9baf07208eca4128e9a7356a3e2ae87b3366
Camilovelag <camilo.vela.galindo@gmail.com>
2023-01-21 13:47:04 -0500

    Add validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  validates :name, presence: true, allow_blank: false
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+
   def last_three_posts
     posts.last(3)
   end
