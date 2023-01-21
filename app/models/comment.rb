a40d379de8445cf440c3e63a9486022d34b6429e
Camilovelag <camilo.vela.galindo@gmail.com>
2023-01-20 19:45:03 -0500

    Make changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post, counter_cache: :comments_counter
+  belongs_to :post
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,13 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', counter_cache: :posts_counter
+  belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
 
   def last_five_comments
     comments.last(5)
+  end
+
+  def update_post_counter
+    author.increment!(:posts_counter)
   end
 end
