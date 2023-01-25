63939bb487abb148b670a31ba2cfa791a486ecce
Camilovelag <camilo.vela.galindo@gmail.com>
2023-01-25 13:38:04 -0500

    Add specs for Posts#index

--- spec/requests/request_spec.rb
+++ spec/requests/request_spec.rb
@@ -39,3 +39,24 @@ RSpec.describe 'Users', type: :request do
     end
   end
 end
+
+RSpec.describe 'Posts', type: :request do
+  context '#index' do
+    before do
+      get user_posts_path(1)
+    end
+
+    it 'should send a 200 response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render a index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'body should contain an specific text' do
+      placeholder = 'Hi! I&apos;m a index posts for an specific user page'
+      expect(response.body).to include(placeholder)
+    end
+  end
+end
