61f6ebabeb5b18f166b0dc130d7c45af4232b91e
Camilovelag <camilo.vela.galindo@gmail.com>
2023-01-25 13:17:48 -0500

    Add specs for Users#show

--- spec/requests/request_spec.rb
+++ spec/requests/request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  context '#index ' do
+  context '#index' do
     before do
       get users_path
     end
@@ -16,6 +16,25 @@ RSpec.describe 'Users', type: :request do
 
     it 'body should contain an specific text' do
       placeholder = 'Hi! I&apos;m an index users page'
+      expect(response.body).to include(placeholder)
+    end
+  end
+
+  context '#show' do
+    before do
+      get user_path(1)
+    end
+
+    it 'should send a 200 response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render a show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'body should contain an specific text' do
+      placeholder = 'Hi! I&apos;m a show an specific user page'
       expect(response.body).to include(placeholder)
     end
   end
